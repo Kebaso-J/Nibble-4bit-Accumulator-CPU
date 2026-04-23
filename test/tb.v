8dea8be12879f079c7c4388c5e67c525d986a9a4
Kebaso Jacob <104685730+Kebaso-J@users.noreply.github.com>
2026-04-23 16:05:13 +0300

    Update module instantiation in tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_jacob_kebaso_4bit_cpu (
+  tt_um_jacob_kebaso_4bit_cpu user_project(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
